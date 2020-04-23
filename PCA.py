baf1bf70c5e8dff2e4ff85c90556d2ab890668cd
Daniel Rodrigues <dan.araujocr@gmail.com>
2020-04-23 17:10:02 -0400

    Adds estimation of scores with missing value functionalities

--- PCA.py
+++ PCA.py
@@ -1,7 +1,8 @@
-from numpy import min, sum, mean, std, var, insert, array, multiply, where, zeros, append, isnan, nan_to_num, nansum, nanmean
+from numpy import min, sum, mean, std, var, insert, array, multiply, where, zeros, append, isnan, nan_to_num, nansum, nanvar, nanmean, unique
 from numpy.linalg import norm
 from pandas import DataFrame
 from sklearn.model_selection import KFold
+from tf_aux import scores_with_missing_values
 
 """
     This Script is for a function that calculates a PCA model in the standards of the
@@ -32,7 +33,8 @@ from sklearn.model_selection import KFold
 
 
 class PCA:
-    def __init__( self, principal_components = None, cv_splits_number = 7, tol = 1e-12, loop_limit = 100 ):
+
+    def __init__( self, principal_components = None, cv_splits_number = 7, tol = 1e-12, loop_limit = 100, missing_values_method = 'TSR' ):
         
         self.loadings = None #loadings       
         self.principal_components = principal_components # number of principal components to be extracted
@@ -40,15 +42,20 @@ class PCA:
         self.tol = tol # criteria for convergence
         self.loop_limit = loop_limit # maximum number of loops before convergence is decided to be not attainable
         self.q2 = [] # list of cross validation scores
-        self.feature_importances_ = None
+        self.feature_importances_ = None #for scikit learn use with feature selection methods
+        self.omega = None # scores covariance matrix for missing values score estimation
+        self.missing_values_method = missing_values_method
         
-    def fit( self, X, Y = None ):
-        if isinstance( X, DataFrame ) : X = X.to_numpy()
+    def fit( self, X, Y = None, int_call = False ):
+
+        if isinstance( X, DataFrame ) : X = X.to_numpy() #ensuring data in the numpy format
         dataset_incomplete = False 
-        if isnan(sum(X)):
-            dataset_incomplete = True
-        if self.principal_components != None :
-            numberOfLVs = self.principal_components
+        if isnan( sum( X ) ): dataset_incomplete = True #checking if there is missing data on the input X
+
+        #CV = False
+        if self.principal_components != None : 
+            numberOfLVs = self.principal_components 
+            #CV = True
         else :
             numberOfLVs = X.shape[1] #maximum amount of extractable latent variables
             kf = KFold( n_splits = self.cv_splits_number, shuffle = True, random_state = 1 )
@@ -68,13 +75,20 @@ class PCA:
                 counter += 1
                 
                 if dataset_incomplete:
-                    loadings_vec = array( nansum( MatrixX2 * scores_vec, axis = 0 ) / sum( scores_vec ** 2 ), ndmin = 2 )
+
+                    loadings_vec = array( nansum( MatrixX2 * scores_vec, axis = 0 ) / nansum( ( ( ~isnan( X ) * scores_vec ) ** 2 ).T, axis = 1 ), ndmin = 2 ) # equivalent to loadings = scores'*data/scores'*scores
+                    loadings_vec = loadings_vec / norm( loadings_vec ) #normalization of loading vector
+                    new_scores_vec = array( nansum( MatrixX2 * loadings_vec, axis = 1 ) / nansum( ( ( ~isnan( X ) * loadings_vec ) ** 2 ), axis = 1), ndmin = 2 ).T #scores calculation w/ missing data
+                
                 else:
+                    
                     loadings_vec = array( scores_vec.T.dot( MatrixX2 ) / ( scores_vec.T.dot( scores_vec ) ), ndmin = 2 ) #loadings = scores'*data/scores'*scores        
-                loadings_vec = loadings_vec / norm(loadings_vec) #normalization of loading vector
-                new_scores_vec = MatrixX2.dot(loadings_vec.T) #scores calculation
-                conv = sum( (scores_vec - new_scores_vec ) ** 2 )  #scores comparation in between loops to assess convergency
+                    loadings_vec = loadings_vec / norm( loadings_vec ) #normalization of loading vector
+                    new_scores_vec = MatrixX2.dot(loadings_vec.T) #scores calculation
+                
+                conv = sum( ( scores_vec - new_scores_vec ) ** 2 )  #scores comparation in between loops to assess convergency
                 scores_vec = new_scores_vec # old t becomes new t
+
             #After convergency, if the principal components desired quantity is undefined
             #then we check if the Component is in fact significant and keep on until all 
             #components are there            
@@ -83,9 +97,11 @@ class PCA:
                 testq2 = []
                 
                 for train_index, test_index in kf.split( X ):
-                    q2_model = PCA( principal_components = latent_variable )
-                    q2_model.fit( X[ train_index ] )
+
+                    q2_model = PCA( principal_components = latent_variable, missing_values_method = self.missing_values_method )
+                    q2_model.fit( X[ train_index ], int_call = True )
                     testq2.append( q2_model.score( X[ test_index ], X[ test_index ] ) )
+
                 q2_final.append( mean( testq2 ) )
                 
                 if latent_variable > 1 :
@@ -98,27 +114,66 @@ class PCA:
                         
             #if significant, then we add them to the loadings and score matrixes that will be returned as method result
             if latent_variable < 2 :
+
                 self.loadings = loadings_vec
+                final_scores = scores_vec
+
             else:
-                self.loadings = insert( self.loadings, self.loadings.shape[0], loadings_vec, axis=0 )
+
+                self.loadings = insert( self.loadings, self.loadings.shape[0], loadings_vec, axis = 0 )
+                final_scores = insert( final_scores, final_scores.shape[1], scores_vec.T, axis = 1 )
+
+            self.omega = final_scores.T.dot(final_scores) # calculation of the covariance matrix
+             
             #prediction of this model
-            MatrixXModel = ( X.dot( self.loadings.T ) ).dot( self.loadings )
-        self.principal_components = self.loadings.shape[ 0 ]
-        self.feature_importances_ = self.VIPs_calc(X)
+            MatrixXModel = final_scores.dot( self.loadings )
+        self.principal_components = self.loadings.shape[ 0 ]        
+
+        if not int_call : 
+            self.feature_importances_ = self.VIPs_calc(X)
+        
         pass
         
-    def predict( self, X, principal_components = None ): 
+    def predict( self, X, principal_components = None ):
+
         if isinstance( X, DataFrame ) : X = X.to_numpy()
         if principal_components == None : principal_components = self.principal_components
         
-        return ( self.transform( X, principal_components = principal_components ) ).dot( self.loadings[ :principal_components, : ] )
+        preds = ( self.transform( X, principal_components = principal_components ) ).dot( self.loadings[ :principal_components, : ] )
+        
+        return preds
     
     def transform( self, X, principal_components = None ): 
         
         if isinstance( X, DataFrame ) : X = X.to_numpy()            
         if principal_components == None : principal_components = self.principal_components
         
-        result = nan_to_num(X).dot( self.loadings[ :principal_components, : ].T )
+        if isnan( sum( X ) ) :
+            result = zeros( ( X.shape[ 0 ], principal_components ) )
+            X_nan = isnan( X )
+            variables_missing_mask = unique( X_nan, axis = 0 )
+            
+            """
+            if missing_values_method == 'TSR' : 
+                main_inverse_term = pinv( self.loadings.dot( self.loadings.T ).dot( self.omega ).dot( self.loadings ).dot( self.loadings.T ) )
+            elif missing_values_method == 'CMR' : 
+                main_inverse_term = pinv( self.loadings.T.dot( self.omega ).dot( self.loadings ) )
+            """
+            for row_mask in variables_missing_mask :
+                
+                rows_indexes = where( ( X_nan == row_mask ).all( axis = 1 ) )                
+                
+                if sum( row_mask ) == 0 : 
+
+                    result[ rows_indexes, : ] = X[ rows_indexes, :].dot( self.loadings[ :principal_components, : ].T )
+                
+                else :
+                    
+                    result[ rows_indexes, : ] = scores_with_missing_values( self.omega, self.loadings[ : , ~row_mask ], X[ rows_indexes, ~row_mask], 
+                                                                            LVs = principal_components, method = self.missing_values_method )
+                 
+                    
+        else : result = X.dot( self.loadings[ :principal_components, : ].T )
         
         return result
     
@@ -127,8 +182,8 @@ class PCA:
         if isinstance( X, DataFrame ) : X = X.to_numpy()
         
         Y = X
-        ErrorQ2 = Y - self.predict( X )
-        return 1 - var( ErrorQ2 ) / var( X )
+        ErrorQ2 = Y - self.predict( X  )
+        return 1 - nanvar( ErrorQ2 ) / nanvar( X )
 
     def Hotellings_T2( self, X, principal_components = None ):
         
