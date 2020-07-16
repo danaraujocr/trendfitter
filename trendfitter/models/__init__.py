3b241367d9856a2890025dce82a2aa445031cb00
Daniel Rodrigues <dan.araujocr@gmail.com>
2020-07-16 17:05:33 -0400

    Adds the new models to the init files

--- trendfitter/models/__init__.py
+++ trendfitter/models/__init__.py
@@ -1,5 +1,7 @@
 from .PCA import PCA
 from .PLS import PLS
+from .MB_PCA import MB_PCA
+from .MB_PLS import MB_PLS
 from .SMB_PLS import SMB_PLS
 
-__all__ = ['PCA', 'PLS', 'SMB_PLS']
+__all__ = ['PCA', 'PLS', 'MB_PCA', 'MB_PLS','SMB_PLS']
