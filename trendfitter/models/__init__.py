80c2bbdc8fb6940c9f6ba9442b71eb5d063ba8ca
Daniel Rodrigues <dan.araujocr@gmail.com>
2020-07-28 13:49:39 -0400

    takes MB_PLS out of the init file for travis testing

--- trendfitter/models/__init__.py
+++ trendfitter/models/__init__.py
@@ -1,7 +1,6 @@
 from .PCA import PCA
 from .PLS import PLS
 from .MB_PCA import MB_PCA
-from .MB_PLS import MB_PLS
 from .SMB_PLS import SMB_PLS
 
-__all__ = ['PCA', 'PLS', 'MB_PCA', 'MB_PLS','SMB_PLS']
+__all__ = ['PCA', 'PLS', 'MB_PCA', 'SMB_PLS']
